100b823e5dcb0af3f2b18581bf350633fc6fdff7
Jesus Mejia <gdj.mejias@gmail.com>
2023-01-21 13:52:54 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolMagnamentApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
